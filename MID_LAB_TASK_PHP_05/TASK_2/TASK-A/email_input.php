8bd85e0e7e2b301b50ee6bed53462265616c83cc
AsifIqbal-debug <asifiqbalaiub8080@gmail.com>
2021-02-22 14:37:30 +0600

    Lab_task_Php_05

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK-A/email_input.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['submit'])){
+
+		$mail 		=  $_POST['email'];
+		
+
+		if($mail == "" ){
+			echo "Null submission...";
+		}else{
+			echo "Success";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?> 
---
+++ MID_LAB_TASK_PHP_05/TASK_4/TASK-B/submission.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+	$gender =  $_POST['gender'];
+	if($gender == "" ){
+		echo "Null submission...";
+	}else{
+		echo "Success  !!!   ";
+		echo $gender."   is your Gender";
+	}
+
+}
+
